a5d14a36446992b4de1316ec4b17d96a15531b81
Design-05 <378999587@qq.com>
2018-05-29 17:31:36 +0800

    something

--- context.go
+++ context.go
@@ -1,5 +1,17 @@
 package rboot
 
 type Context struct {
+	bot *Rboot
+}
 
+func (ctx *Context) BotName() string {
+	return ctx.bot.name
+}
+
+func (ctx *Context) MessageIn() chan Message {
+	return ctx.bot.providerIn
+}
+
+func (ctx *Context) MessageOut() chan Message {
+	return ctx.bot.providerOut
 }
--- rboot.go
+++ rboot.go
@@ -1,11 +1,57 @@
 package rboot
 
+import (
+	"sync"
+	"log"
+)
+
 type Rboot struct {
+	name string
+	providerIn  chan Message
+	providerOut chan Message
+
 	plugs map[string]Plugin
 }
 
+func NewRboot(name string) *Rboot {
+	return &Rboot{
+		name:        name,
+		providerIn:  make(chan Message),
+		providerOut: make(chan Message),
+	}
+}
+
+var once sync.Once
+
+func (bot *Rboot) Go() {
+	once.Do(func() {
+
+		for in := range bot.providerIn {
+
+			go func(bot Rboot, msg Message) {
+				defer func() {
+					if r := recover(); r != nil {
+						log.Printf("panic recovered when parsing message: %#v. Panic: %v", msg, r)
+					}
+				}()
+				/*for _, plug := range bot.plugs {
+					responses := plug.Action
+					for _, r := range responses {
+						s.providerOut <- r
+					}
+				}*/
+			}(*bot, in)
+		}
+	})
+}
+
+func (bot *Rboot) Name() string {
+	return bot.name
+}
+
 // 适配连接器
-type Connecter interface {
+type Provider interface {
+	Run() error
 	Incoming() chan Message
 	Outgoing() chan Message
 }
