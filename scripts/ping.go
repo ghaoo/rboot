8d53de6d53a899d12ff96521f6ae4cba01de290e
guhao022 <378999587@qq.com>
2018-06-29 17:30:10 +0800

    fix

--- scripts/ping.go
+++ scripts/ping.go
@@ -1,1 +1,32 @@
 package scripts
+
+import (
+	"strings"
+
+	"github.com/ghaoo/rboot"
+)
+
+func setup(bot rboot.Robot, msg rboot.Message) []rboot.Message {
+
+	if bot.MatchMessage(`ping|PING|Ping`, msg) {
+		reg := bot.Regexp(`ping|PING|Ping`)
+
+		fs := reg.FindAllStringSubmatch(msg.Content(), -1)[0]
+
+		println(fs)
+
+		return []rboot.Message{
+			{
+				Body: strings.NewReader(`PONG`),
+			},
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	rboot.RegisterScript(`ping`, &rboot.Script{
+		Action: setup,
+	})
+}
