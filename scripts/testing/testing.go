981c9bb73369f391da5dc364617a2d42ddce97ba
guhao022 <378999587@qq.com>
2018-06-29 12:12:17 +0800

    fix

--- scripts/testing/testing.go
+++ scripts/testing/testing.go
@@ -5,11 +5,12 @@ import (
 	"time"
 
 	"github.com/ghaoo/rboot"
+	"strings"
 )
 
 func setup(bot rboot.Robot, msg rboot.Message) []rboot.Message {
 
-	if msg.Content == `11` {
+	/*if msg.Content == `11` {
 		return []rboot.Message{
 			{Content: `22222222222222222`},
 		}
@@ -18,7 +19,7 @@ func setup(bot rboot.Robot, msg rboot.Message) []rboot.Message {
 		return []rboot.Message{
 			{Content: `11111111111111111`},
 		}
-	}
+	}*/
 
 	return nil
 }
@@ -35,13 +36,13 @@ time: %v
 count: %d
 `, time.Now().Local(), data.Count)
 
-		bot.Send(rboot.Message{Content: str})
+		bot.Send(rboot.Message{Body: strings.NewReader(str)})
 	})
 
 	bot.Timing(`15:14`)
 	bot.Handle(`/timing/15:13`, func(evt rboot.Event) {
 
-		bot.Send(rboot.Message{Content: time.Now().Local().String()})
+		bot.Send(rboot.Message{Body: strings.NewReader(time.Now().Local().String())})
 	})
 }
 
