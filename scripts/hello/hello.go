68adc908cfc1ca2994be744a8f6989c10d34d6fc
guhao022 <378999587@qq.com>
2020-01-16 15:25:40 +0800

    fix

---
+++ scripts/hello/hello.go
@@ -0,0 +1,14 @@
+package hello
+
+import "github.com/ghaoo/rboot"
+
+func init() {
+	// 注册脚本
+	rboot.RegisterScripts(`hello`, rboot.Script{
+		// 脚本处理函数
+		Action: func(bot *rboot.Robot, incoming *rboot.Message) []*rboot.Message {
+			return rboot.NewMessages("Hello World!")
+		},
+		Ruleset: map[string]string{`hello`: `^hello`}, // 脚本规则集
+	})
+}
--- scripts/scripts.go
+++ scripts/scripts.go
@@ -1,6 +1,7 @@
 package scripts
 
 import (
+	_ "github.com/ghaoo/rboot/scripts/hello"
 	_ "github.com/ghaoo/rboot/scripts/ping"
 	_ "github.com/ghaoo/rboot/scripts/vote"
 
