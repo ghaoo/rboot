36cc28b5b075cf8cc49ae324675a372f363d080b
guhao022 <378999587@qq.com>
2020-11-05 14:34:32 +0800

    fix

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -3,6 +3,5 @@ package adapter
 import (
 	_ "github.com/ghaoo/rboot/adapter/bearychat"
 	_ "github.com/ghaoo/rboot/adapter/dingtalk"
-	_ "github.com/ghaoo/rboot/adapter/wechat"
 	_ "github.com/ghaoo/rboot/adapter/wework"
 )
